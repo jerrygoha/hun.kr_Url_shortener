629c8f60cae6e47dd77e4b5883d0a80725926189
Earth_Science_Teacher <jerrygoha@naver.com>
2021-11-04 07:26:28 +0900

    (modified) front code modified

--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,57 @@
 $(function() {
 	"use strict";
 	 $('#submitButton').click(function () {
-            $.ajax({
-                type: "POST",
-                url: "/reduce/",
-                data: {
-                    'url': $('#url').val(),
-                    'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
-                },
-                success: returnSuccess,
-                dataType: 'json'
-            });
+	 	var isUrl = checkUrlValid($('#url').val());
+	 	if (isUrl) {
+			$.ajax({
+				type: "POST",
+				url: "/reduce/",
+				data: {
+					'url': $('#url').val(),
+					'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
+				},
+				success: returnSuccess,
+				dataType: 'json'
+			});
+		}else{
+	 		$('#url-result').text("잘못 입력하셨습니다. 정상적인 url을 입력해주세요.")
+		}
 	 });
 
+	 // 결과 출력
 	 function returnSuccess(data, textStatus, jqXHR) {
         if (data.url) {
             $('#url-result').text(data.url);
             $('#url').val("");
-        } else {
-            $('#url-result').text("Do not submit blank.");
         }
-
     }
+
+    // url 유효성 검사
+    function checkUrlValid(input){
+	 	var input_url = input;
+	 	var UrlCheck = /((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\S*))*$/i;
+	 	var urlTest = UrlCheck.test(input_url);
+
+	 	return urlTest
+	}
+
 	var	$body = document.querySelector('body');
-
 	// Methods/polyfills.
-
 		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
 			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
-
 		// canUse
 			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
-
 		// window.addEventListener
 			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
 
-	// Play initial animations on page load.
+	// 페이드인 애니메이션
 		window.addEventListener('load', function() {
 			window.setTimeout(function() {
 				$body.classList.remove('is-preload');
 			}, 100);
 		});
 
-	// Slideshow Background.
+	// 배경화면 전환
 		(function() {
 			// Settings.
 				var settings = {
@@ -53,7 +62,7 @@ $(function() {
 							'static/images/bg03.jpg': 'center'
 						},
 					// Delay.
-						delay: 6000
+						delay: 4000
 				};
 			// Vars.
 				var	pos = 0, lastPos = 0,
@@ -139,16 +148,12 @@ $(function() {
 
 			// Events.
 				$form.addEventListener('submit', function(event) {
-
 					event.stopPropagation();
 					event.preventDefault();
-
 					// Hide message.
 						$message._hide();
-
 					// Disable submit.
 						$submit.disabled = true;
-
 					// Process form.
 						window.setTimeout(function() {
 							// Reset form.
@@ -157,9 +162,7 @@ $(function() {
 								$submit.disabled = false;
 							// Show message.
 								$message._show('success', 'Done!');
-
 						}, 750);
-
 				});
 		})();
 })();
