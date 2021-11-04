918b000014d29644cc9eb374681475b3f95da939
Earth_Science_Teacher <jerrygoha@naver.com>
2021-11-04 11:59:43 +0900

    (modified) 코드 품질 개선
    PEP 8 참고하여 품질개선작업중. 주석 수정 및 불필요한 로직 정리

--- urlShortener/views.py
+++ urlShortener/views.py
@@ -5,7 +5,6 @@ import logging
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 from .models import UrlHistory
@@ -15,53 +14,48 @@ from .models import UrlHistory
 def homepage(request):
     return render(request, 'urlShortener/index.html')
 
+
 # redirect function
 def redirect_originalUrl(request, hashed_id):
-    url = get_object_or_404(UrlHistory, url_IdHash=hashed_id)
+    base10_id = __make_base10(hashed_id)
+    url = get_object_or_404(UrlHistory, id=base10_id)
     return HttpResponseRedirect(url.url_originalAddr)
 
-# hashing and create function
-"""
-<1.먼저 두 형태로 hashing한다.>
-ㅣ---- url hashing : md5 hashing 사용 (동일한 입력값에 대해 결과가 항상 같기때문.)
-ㅣ---- id hashing : base62 hashing 사용
-why? : url은 길이가 굉장히 길기때문에 crc32, md5, 또는 커스텀 알고리즘을 만들어 해싱하는 경우
-        결과값이 중복될 가능성이 있다.
-        하지만, id값(int)은 base62(1~9, a~z, A~Z)를 사용하여 8자리 이내에서 중복없이 표현이 가능하다.
-        단, 일어날 수 있는 동시성 문제에 대해서는 실제 서비스에 미치는 영향이 미미하다 판단하여 고려하지 않는다.
 
-<2. url 중복체크 (md5)>
-db 내에서 긴 길이의 url끼리 중복체크를 하는것보다, 해싱된 문자를 비교하는게 비용이 더 저렴.
-다만, 다른 url끼리 해싱값이 같을 수 있으니, 해싱값이 같은 경우 original_url도 중복체크한다.(확률이 낮으므로 큰 비용 소모되지 않을듯하다.)
-해싱값이 같고, original_url도 같다면 완전히 중복되므로, 기존 original_url의 id 해싱값을 리턴한다.
-"""
 def hashingUrl(request):
+    """입력받은 url을 줄이는 기능"""
+
     original_url = __rebuild_Url(request.POST.get("url", ''))
-    if not (original_url == ""):
-        url_UrlHash = __make_md5(original_url)
-        #입력받은 주소가 db에 존재하는지 체크
-        try:
-            _url = UrlHistory.objects.get(url_UrlHash=url_UrlHash)
-        except:
-            _url = None
 
-        # 새로운 주소일 경우 url과 id의 해시값을 db에 저장
-        if _url is None: 
-            db_input = UrlHistory(url_originalAddr=original_url, url_UrlHash=url_UrlHash)
-            db_input.save()
+    # if not (original_url == ""):
+    url_UrlHash = __make_md5(original_url)
 
-            # save hashed_id
-            temp_obj = UrlHistory.objects.get(url_originalAddr=original_url)
-            id_IdHash = __make_base62(temp_obj.id)
-            temp_obj.url_IdHash = id_IdHash
-            temp_obj.save()
-        # 이미 등록되어있는 주소일 경우 단순반환
-        else: 
-            id_IdHash = UrlHistory.objects.get(url_originalAddr=original_url).url_IdHash
-        response = {}
-        response["url"] = settings.SITE_URL + "/" + id_IdHash
-        return HttpResponse(json.dumps(response), content_type="application/json")
-    return HttpResponse(json.dumps({"error": "error!!"}), content_type="application/json")
+    # 입력받은 주소가 db에 존재하는지 체크
+    try:
+        _url = UrlHistory.objects.get(url_UrlHash=url_UrlHash)
+    except:
+        _url = None
+
+    # 새로운 주소일 경우 url과 id의 해시값을 db에 저장
+    if _url is None:
+        db_input = UrlHistory(url_originalAddr=original_url, url_UrlHash=url_UrlHash)
+        db_input.save()
+
+        # db에 저장 이후 생긴 id값을 가져와 id_hash값 도출
+        temp_obj = UrlHistory.objects.get(url_originalAddr=original_url)
+        id_IdHash = __make_base62(temp_obj.id)
+        # db에 저장하게된다면 살릴 코드
+        # temp_obj.url_IdHash = id_IdHash
+        # temp_obj.save()
+
+    # 이미 등록되어있는 주소일 경우 단순반환
+    else:
+        id_IdHash = __make_base62(UrlHistory.objects.get(url_originalAddr=original_url).id)
+
+    response = {}
+    response["url"] = settings.SITE_URL + "/" + id_IdHash
+    return HttpResponse(json.dumps(response), content_type="application/json")
+# return HttpResponse(json.dumps({"error": "error!!"}), content_type="application/json")
 
 
 def __make_md5(original_url):
@@ -85,6 +79,7 @@ def __make_base62(url_id):
         result = base62_char[int(char_cnt)] + result
     return result
 
+
 def __make_base10(input):
     """base62 id_hash 를 10진수로 디코딩"""
     base62_char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -104,8 +99,9 @@ def __rebuild_Url(input):
     if input_url[-1] is "/":
         input_url = input_url[:-1]
 
-    if "http://" in input_url or "https://" in input_url:
+    if "http://" in input_url:
         return input_url
+    elif "https://" in input_url:
+        return "http://" + input_url[8:]
     else:
-        return "http://"+input_url
-
+        return "http://" + input_url
