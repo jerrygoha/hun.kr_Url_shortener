20cec094e5ebbcb9301b6d207b89d37b035be15a
Earth_Science_Teacher <jerrygoha@naver.com>
2021-11-04 10:34:43 +0900

    (modified) 1~8자리 문자열만 인식하도록 정규식 수정
    sqlite integer 최대값이 "천조"단위를 넘으므로 62^8 자리까지로 제한

--- urlShortener/urls.py
+++ urlShortener/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', homepage, name='main'),
-    url(r'^(?!^admin|^reduce)(?P<hashed_id>.*)$', redirect_originalUrl, name='redirectOriginalUrl' ),
-    url(r'^reduce/$', hashingUrl, name='hashing' )
+    # admin, reduce로 시작하지 않고, 1자리부터 8자리 사이의 자릿수로 base62 문자가 들어가면 매치.
+    url(r'^(?!^admin|^shorten)(?P<hashed_id>.{1,8})$', redirect_originalUrl, name='redirectOriginalUrl' ),
+    url(fr'^shorten/$', hashingUrl, name='hashing' )
 ]
